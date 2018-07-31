2608e9ce1d160081be34b02c782e1052910158f8
Ryan Florence <rpflorence@gmail.com>
2018-07-31 07:31:59 -0700

    use same package name on window in UMD build

--- shared/rollup-config.js
+++ shared/rollup-config.js
@@ -11,7 +11,7 @@ let pkg = path.basename(process.env.PWD);
 const config = {
   input: "src/index.js",
   output: {
-    name: "Reach" + inflection.classify(pkg),
+    name: `@reach/${pkg}`,
     globals: {
       react: "React",
       "react-dom": "ReactDOM"
