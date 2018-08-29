d762fc122a371ec059c2c2d45f2358d34aabf999
Patrik Vormittag <pvormittag@engrain.com>
2018-08-29 08:39:00 -0600

    Fix memory leak with event handler in WindowSize component.

--- packages/window-size/src/index.js
+++ packages/window-size/src/index.js
@@ -11,7 +11,7 @@ let didMount = ({ refs, setState }) => {
     });
   window.addEventListener("resize", resize);
   refs.removeEvent = () => {
-    window.addEventListener("resize", resize);
+    window.removeEventListener("resize", resize);
   };
 };
 
