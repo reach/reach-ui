9c1b59dfaa705826d8e1b0e9652358851082751a
Kevin Donahue <nonnontrivial@gmail.com>
2018-09-11 14:44:09 -0400

    Add rendering tests

--- packages/component-component/src/index.test.js
+++ packages/component-component/src/index.test.js
@@ -17,10 +17,40 @@ describe("rendering", () => {
       </Component>
     );
   });
-  // it("prefers `render` over `children`");
-  // it("renders without children");
-  // it("renders with normal children");
-  // it("renders with children render prop");
+  it("prefers `render` over `children`", () => {
+    let { children } = snapshot(
+      <Component
+        initialState={{ value: "!" }}
+        render={({ state }) => <div>{state.value}</div>}
+      >
+        <div>!!</div>
+      </Component>
+    );
+    expect(children).toEqual(["!"]);
+  });
+  it("renders without children", () => {
+    let { root } = renderer.create(<Component initialState={{ value: "!" }} />);
+    expect(root.props.initialState.value).toBe("!");
+  });
+  it("renders with normal children", () => {
+    let { root } = renderer.create(
+      <Component>
+        <div>
+          <div value="y">!</div>
+        </div>
+      </Component>
+    );
+    expect(root.findByProps({ value: "y" })).toBeTruthy();
+  });
+  it("renders with children render prop", () => {
+    let { children } = snapshot(
+      <Component
+        initialState={{ value: "!" }}
+        render={({ state }) => <div>{state.value}</div>}
+      />
+    );
+    expect(children).toEqual(["!"]);
+  });
 });
 
 // describe("refs", () => {
