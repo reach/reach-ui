21e5ee896f02da6bcd86932550d245b2a6094234
Luke Glazebrook <Glazy@users.noreply.github.com>
2018-09-19 00:15:40 +0100

    Update story and add caret char to button

--- packages/disclosure/examples/basic.example.js
+++ packages/disclosure/examples/basic.example.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Disclosure from "../src/index";
+import "../styles.css";
 
 export let name = "Basic";
 
-export let Example = () => <Disclosure>Hello world</Disclosure>;
+export let Example = () => (
+  <Disclosure buttonLabel={"Friendly message"}>
+    <p>
+      Here is a friendly message that was hidden away by the disclosure
+      component.
+    </p>
+    <a href="https://reach.tech/ui">
+      Here is a link back to the Reach UI site homepage.
+    </a>
+  </Disclosure>
+);
--- packages/disclosure/src/index.js
+++ packages/disclosure/src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Component from "@reach/component-component";
+import PropTypes from "prop-types";
 
-class Disclosure extends Component {
-  render() {
-    return <h1>Hello world</h1>;
-  }
-}
+const Disclosure = ({ buttonLabel, children }) => (
+  <Component getInitialState={() => ({ isExpanded: false })}>
+    {({ state, setState }) => (
+      <React.Fragment>
+        <button
+          aria-expanded={state.isExpanded}
+          data-reach-disclosure-button
+          onClick={() =>
+            setState(({ isExpanded }) => ({ isExpanded: !isExpanded }))
+          }
+        >
+          <span aria-hidden={true}>{state.isExpanded ? "▼" : "►"}</span>
+          {buttonLabel}
+        </button>
+        {state.isExpanded && <div>{children}</div>}
+      </React.Fragment>
+    )}
+  </Component>
+);
+
+Disclosure.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+};
 
 export default Disclosure;
