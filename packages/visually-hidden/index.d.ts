e8e0866a9c10da3f897d53eb90d4484490804bd4
sunnibfgi <sunqijun1523@gmail.com>
2019-01-14 15:58:55 +0800

    check the type of the children props

---
+++ packages/visually-hidden/index.d.ts
@@ -0,0 +1,8 @@
+declare module "@reach/visually-hidden" {
+  interface VisuallyHiddenProps {
+    children: React.ReactNode;
+  }
+  const VisuallyHidden: React.SFC<VisuallyHiddenProps>;
+  
+  export default VisuallyHidden;
+}
