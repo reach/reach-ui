88dbb0f2861dd1a1d95c9059af97f6ffe53eed90
Ryan Florence <rpflorence@gmail.com>
2019-04-19 08:40:18 -0700

    [tooltip]: added!

---
+++ packages/tooltip/examples/animated.example.js
@@ -0,0 +1,65 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import "../styles.css";
+import React, { Fragment, cloneElement } from "react";
+import { useTooltip, TooltipPopup } from "../src/index";
+import { useTransition, animated } from "react-spring";
+
+export const name = "Animated";
+
+animated.TooltipPopup = animated(TooltipPopup);
+animated.TooltipContent = animated(TooltipPopup);
+
+function AnimatedTooltip({ children, ...rest }) {
+  const [trigger, tooltip, isVisible] = useTooltip();
+
+  const transitions = useTransition(isVisible ? tooltip : false, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { mass: 1, tension: 500, friction: 40 }
+  });
+
+  return (
+    <Fragment>
+      {cloneElement(children, trigger)}
+
+      {transitions.map(
+        ({ item: tooltip, props: styles, key }) =>
+          tooltip && (
+            <animated.TooltipContent
+              key={key}
+              {...tooltip}
+              {...rest}
+              style={styles}
+            />
+          )
+      )}
+    </Fragment>
+  );
+}
+
+export function Example() {
+  return (
+    <div>
+      <AnimatedTooltip label="Notifications">
+        <button style={{ fontSize: 25 }}>
+          <span aria-hidden>🔔</span>
+        </button>
+      </AnimatedTooltip>
+      <AnimatedTooltip label="Settings">
+        <button style={{ fontSize: 25 }}>
+          <span aria-hidden>⚙️</span>
+        </button>
+      </AnimatedTooltip>
+
+      <div style={{ float: "right" }}>
+        <AnimatedTooltip label="Notifications" ariaLabel="3 Notifications">
+          <button style={{ fontSize: 25 }}>
+            <span>🔔</span>
+            <span>3</span>
+          </button>
+        </AnimatedTooltip>
+      </div>
+    </div>
+  );
+}
