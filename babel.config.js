dc43f719e2d2e7480bbc13860742f1764c7881d0
Michael Jackson <michael@jackson.us>
2019-09-05 23:06:02 -0700

    Upgrade to Babel 7 and Storybook 5

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+// TODO: This isn't what BABEL_ENV is for. Once we are using
+// Rollup for builds (using rollup-plugin-babel) we won't need
+// this anymore anyway.
+const moduleFormat = process.env.MODULE_FORMAT || "esm";
+
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        modules: moduleFormat === "esm" ? false : moduleFormat
+      }
+    ],
+    "@babel/preset-react"
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    "babel-plugin-dev-expression"
+  ]
+};
