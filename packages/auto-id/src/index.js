9aa03f83652ed887998b8ee98f7d7baa84494de2
chancestrickland <chance.strickland@gmail.com>
2019-10-03 10:23:33 -0700

    auto-id: rewrite with react-uid

--- packages/auto-id/src/index.js
+++ packages/auto-id/src/index.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react";
+import { useUIDSeed } from "react-uid";
 
-// Could use UUID but if we hit 9,007,199,254,740,991 unique components over
-// the lifetime of the app before it gets reloaded, I mean ... come on.
-// I don't even know what xillion that is.
-// /me googles
-// Oh duh, quadrillion. Nine quadrillion components. I think we're okay.
-let id = 0;
-const genId = () => ++id;
+// strips trailing '-' if postfix is an empty string
+const clean = (string = "") => string.replace(/-$/, "");
 
-export const useId = () => {
-  const [id, setId] = useState(null);
-  useEffect(() => setId(genId()), []);
-  return id;
+export const useId = (postfix = "") => {
+  const seed = useUIDSeed();
+  if (typeof postfix !== "string" && typeof postfix !== "number") {
+    console.warn(
+      `The argument passed to useId must be either a string or a number, but a ${typeof postfix} was detected. A random string has been generated as a fallback which may break the accessibility of your application depending on how the hook is used. See [LINK TO DOCS] for details.`
+    );
+    postfix = "";
+  }
+  return !postfix ? clean(seed(postfix)) : seed(postfix);
 };
