636cf9db6b21e9d0ae01336322a6447d0067052b
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-10-11 23:58:37 +0200

    Avoid extra render
    Technically this is a side effect outside 
    a `useEffect`, but it avoids the extra render
    triggered by the `setState` of the current implementation
    
    One use case in which the extra render is bad is for example when wrapping an external library
    ```
    function Editor() {
    	const id = useId()
    	const editorId = `editor_${id}`
    	useEffect(() => {
    		const editor = new ExampleJQueryEditor(editorId)
    	}) 
    	
    	return <div id={editorId} />
    }
    ```
    
    Not sure if this will cause issues in concurrent mode.

--- packages/auto-id/src/index.js
+++ packages/auto-id/src/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 
 // Could use UUID but if we hit 9,007,199,254,740,991 unique components over
 // the lifetime of the app before it gets reloaded, I mean ... come on.
@@ -6,10 +6,7 @@ import { useState, useEffect } from "react";
 // /me googles
 // Oh duh, quadrillion. Nine quadrillion components. I think we're okay.
 let id = 0;
-const genId = () => ++id;
 
 export const useId = () => {
-  const [id, setId] = useState(null);
-  useEffect(() => setId(genId()), []);
-  return id;
+  return useMemo(() => id++, [])
 };
