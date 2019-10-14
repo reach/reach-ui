e0acd6f59a08daab3d32df851c6ba946fc11328c
chancestrickland <chance.strickland@gmail.com>
2019-10-13 22:16:51 -0700

    portal: revert to component-component

--- packages/portal/src/index.js
+++ packages/portal/src/index.js
@@ -1,34 +1,30 @@
 import React from "react";
 import { createPortal } from "react-dom";
+import Component from "@reach/component-component";
 
-let Portal = ({ children, type = "reach-portal" }) => {
-  const mountNode = React.useRef(null);
-  const portalNode = React.useRef(null);
-  const forceUpdate = useForceUpdate();
-  React.useEffect(() => {
-    // It's possible that the content we are portal has, itself, been portaled.
-    // In that case, it's important to append to the correct document element.
-    const ownerDocument = mountNode.current.ownerDocument;
-    portalNode.current = ownerDocument.createElement(type);
-    forceUpdate();
-    return () => {
-      portalNode.current.ownerDocument.body.removeChild(portalNode.current);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [type]);
-  return !portalNode.current ? (
-    <div ref={mountNode} />
-  ) : (
-    createPortal(children, portalNode.current)
-  );
-};
+let Portal = ({ children, type = "reach-portal" }) => (
+  <Component
+    getRefs={() => ({ mountNode: null, portalNode: null })}
+    didMount={({ refs, forceUpdate }) => {
+      // It's possible that the content we are portal has, itself, been portaled.
+      // In that case, it's important to append to the correct document element.
+      const ownerDocument = refs.mountNode.ownerDocument;
+      refs.portalNode = ownerDocument.createElement(type);
+      ownerDocument.body.appendChild(refs.portalNode);
+      forceUpdate();
+    }}
+    willUnmount={({ refs: { portalNode } }) => {
+      portalNode.ownerDocument.body.removeChild(portalNode);
+    }}
+    render={({ refs }) => {
+      const { portalNode } = refs;
+      if (!portalNode) {
+        return <div ref={div => (refs.mountNode = div)} />;
+      } else {
+        return createPortal(children, portalNode);
+      }
+    }}
+  />
+);
 
 export default Portal;
-
-function useForceUpdate() {
-  const [, dispatch] = React.useState(Object.create(null));
-  const update = React.useCallback(() => {
-    dispatch(Object.create(null));
-  }, [dispatch]);
-  return update;
-}
