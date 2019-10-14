0c29edb21e0c7a18fd422dc83e0e3b4a902cc115
Chance Strickland <chance@reacttraining.com>
2019-10-13 22:34:17 -0700

    Component² > hooks (#301)
    * tooltip: add dusabled button example
    
    * alert: rework example with hooks
    
    * refactor assignRef to use hook for consistency
    
    * dialog: refactor to remove component-component
    
    * useForkedRef fixes
    
    * alert-dialog: remove component-component
    
    * remove unused import
    
    * alert: fix hidden root alerts
    
    * dialog: rm component-component from examples
    
    * portal: remove component-component
    
    * rect: remove component-component
    
    * docs: add onChange to useRect docs
    
    * rect: clean dependencies
    
    * window-size: remove component-component
    
    * portal: revert to component-component
    
    * normalize package author field

--- packages/dialog/examples/dropdown.example.js
+++ packages/dialog/examples/dropdown.example.js
@@ -1,33 +1,26 @@
 import React from "react";
-import Component from "@reach/component-component";
 import { Menu, MenuButton, MenuList, MenuItem } from "@reach/menu-button";
 import "../styles.css";
 import { Dialog } from "@reach/dialog";
 
 export let name = "Dropdown";
 
-export let Example = () => (
-  <Component initialState={{ showDialog: false }}>
-    {({ state, setState }) => (
-      <div>
-        <button onClick={() => setState({ showDialog: true })}>
-          Show Dialog
-        </button>
-
-        <Dialog isOpen={state.showDialog}>
-          <button onClick={() => setState({ showDialog: false })}>
-            Close Dialog
-          </button>
-          <Menu>
-            <MenuButton>Dropdown</MenuButton>
-            <MenuList>
-              <MenuItem>Test</MenuItem>
-              <MenuItem>Test</MenuItem>
-              <MenuItem>Test</MenuItem>
-            </MenuList>
-          </Menu>
-        </Dialog>
-      </div>
-    )}
-  </Component>
-);
+export let Example = () => {
+  const [showDialog, setShowDialog] = React.useState(false);
+  return (
+    <div>
+      <button onClick={() => setShowDialog(true)}>Show Dialog</button>
+      <Dialog isOpen={showDialog}>
+        <button onClick={() => setShowDialog(false)}>Close Dialog</button>
+        <Menu>
+          <MenuButton>Dropdown</MenuButton>
+          <MenuList>
+            <MenuItem>Test</MenuItem>
+            <MenuItem>Test</MenuItem>
+            <MenuItem>Test</MenuItem>
+          </MenuList>
+        </Menu>
+      </Dialog>
+    </div>
+  );
+};
