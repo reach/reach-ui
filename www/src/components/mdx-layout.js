1caace7d95db0ee9f4c2e31abf8c45857b2cbc34
chancestrickland <chance.strickland@gmail.com>
2019-10-15 12:35:16 -0700

    docs: update react-live widgets

--- www/src/components/mdx-layout.js
+++ www/src/components/mdx-layout.js
@@ -1,38 +1,39 @@
-import "./normalize.css"
-import "./skeleton.css"
-import "./syntax.css"
-import "./app.css"
+import "./normalize.css";
+import "./root.css";
+import "./skeleton.css";
+import "./syntax.css";
+import "./app.css";
 
-import React from "react"
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live"
-import { MDXProvider } from "@mdx-js/react"
-import Layout from "./layout"
-import { useThrottle } from "use-throttle"
-import matchSorter from "match-sorter"
+import React from "react";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { MDXProvider } from "@mdx-js/react";
+import Layout from "./layout";
+import { useThrottle } from "use-throttle";
+import matchSorter from "match-sorter";
 
-import GatsbyLink from "gatsby-link"
+import GatsbyLink from "gatsby-link";
 
-import Component from "@reach/component-component"
-import Rect, { useRect } from "@reach/rect"
-import WindowSize from "@reach/window-size"
-import Portal from "@reach/portal"
-import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog"
+import Component from "@reach/component-component";
+import Rect, { useRect } from "@reach/rect";
+import WindowSize, { useWindowSize } from "@reach/window-size";
+import Portal from "@reach/portal";
+import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
 import {
   Menu,
   MenuList,
   MenuButton,
   MenuItem,
-  MenuLink,
-} from "@reach/menu-button"
-import VisuallyHidden from "@reach/visually-hidden"
-import Alert from "@reach/alert"
+  MenuLink
+} from "@reach/menu-button";
+import VisuallyHidden from "@reach/visually-hidden";
+import Alert from "@reach/alert";
 import {
   AlertDialog,
   AlertDialogLabel,
   AlertDialogDescription,
   AlertDialogOverlay,
-  AlertDialogContent,
-} from "@reach/alert-dialog"
+  AlertDialogContent
+} from "@reach/alert-dialog";
 
 import {
   Combobox,
@@ -40,108 +41,114 @@ import {
   ComboboxPopover,
   ComboboxList,
   ComboboxOption,
-  ComboboxOptionText,
-} from "@reach/combobox"
+  ComboboxOptionText
+} from "@reach/combobox";
 
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs"
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
 
 import {
   Slider,
   SliderTrack,
   SliderTrackHighlight,
   SliderHandle,
-  SliderMarker,
-} from "@reach/slider"
+  SliderMarker
+} from "@reach/slider";
 
-import { useId } from "@reach/auto-id"
+import { useId } from "@reach/auto-id";
 
-import Tooltip, { useTooltip, TooltipPopup } from "@reach/tooltip"
+import Tooltip, { useTooltip, TooltipPopup } from "@reach/tooltip";
 
-import { useTransition, animated } from "react-spring/web.cjs"
+import { useTransition, animated } from "react-spring/web.cjs";
 
-import { Phased } from "recondition"
+import { Phased } from "recondition";
 
 const PreComponent = ({ className, ...props }) => {
-  console.log(props.children.props)
-  return props.children.props &&
-    props.children.props.className === "language-jsx" ? (
-    <LiveProvider
-      theme={{ plain: {}, styles: [] }}
-      code={props.children.props.children.trim()}
-      //code={props.children.trim()}
-      // ode={props.children}
-      scope={{
-        ...React,
-        animated,
-        GatsbyLink,
-        Component,
-        Rect,
-        useRect,
-        WindowSize,
-        Portal,
-        Menu,
-        MenuList,
-        MenuButton,
-        MenuItem,
-        MenuLink,
-        VisuallyHidden,
-        Dialog,
-        DialogOverlay,
-        DialogContent,
-        useTransition,
-        Phased,
-        Alert,
-        AlertDialog,
-        AlertDialogDescription,
-        AlertDialogLabel,
-        AlertDialogOverlay,
-        AlertDialogContent,
-        Tabs,
-        TabList,
-        Tab,
-        TabPanels,
-        TabPanel,
-        Slider,
-        SliderTrack,
-        SliderTrackHighlight,
-        SliderHandle,
-        SliderMarker,
-        useId,
-        Tooltip,
-        TooltipPopup,
-        useTooltip,
-        Combobox,
-        ComboboxInput,
-        ComboboxPopover,
-        ComboboxList,
-        ComboboxOption,
-        ComboboxOptionText,
-        useThrottle,
-        matchSorter,
-      }}
-    >
-      <LiveEditor className="shit" tabIndex="-1" />
-      <LiveError />
-      <LivePreview />
-    </LiveProvider>
+  const isLive =
+    props.children &&
+    props.children.props &&
+    props.children.props.className &&
+    props.children.props.className === "language-jsx-live";
+  return isLive ? (
+    <div className="react-live">
+      <LiveProvider
+        theme={{ plain: {}, styles: [] }}
+        code={props.children.props.children.trim()}
+        //code={props.children.trim()}
+        // ode={props.children}
+        scope={{
+          ...React,
+          animated,
+          GatsbyLink,
+          Component,
+          Rect,
+          useRect,
+          WindowSize,
+          useWindowSize,
+          Portal,
+          Menu,
+          MenuList,
+          MenuButton,
+          MenuItem,
+          MenuLink,
+          VisuallyHidden,
+          Dialog,
+          DialogOverlay,
+          DialogContent,
+          useTransition,
+          Phased,
+          Alert,
+          AlertDialog,
+          AlertDialogDescription,
+          AlertDialogLabel,
+          AlertDialogOverlay,
+          AlertDialogContent,
+          Tabs,
+          TabList,
+          Tab,
+          TabPanels,
+          TabPanel,
+          Slider,
+          SliderTrack,
+          SliderTrackHighlight,
+          SliderHandle,
+          SliderMarker,
+          useId,
+          Tooltip,
+          TooltipPopup,
+          useTooltip,
+          Combobox,
+          ComboboxInput,
+          ComboboxPopover,
+          ComboboxList,
+          ComboboxOption,
+          ComboboxOptionText,
+          useThrottle,
+          matchSorter
+        }}
+      >
+        <LiveEditor className="react-live-editor" tabIndex={-1} />
+        <LiveError className="react-live-error" />
+        <LivePreview className="react-live-preview" />
+      </LiveProvider>
+    </div>
   ) : (
-    <pre {...props} className="WHAT_THE_CRAP" />
-  )
-}
+    <pre {...props} />
+  );
+};
 
-const Table = props => <table className="u-full-width" {...props} />
+const Table = props => <table className="u-full-width" {...props} />;
 
-let firstLoad = true
+let firstLoad = true;
 
 export default class MyPageLayout extends React.Component {
   componentDidMount() {
     if (firstLoad) {
-      firstLoad = false
+      firstLoad = false;
     } else {
-      this.node.focus()
+      this.node.focus();
     }
     // I dunno, I just made it global on window, whatever...
-    import("./cities.js")
+    import("./cities.js");
   }
 
   render() {
@@ -158,6 +165,6 @@ export default class MyPageLayout extends React.Component {
           </main>
         </MDXProvider>
       </Layout>
-    )
+    );
   }
 }
