077ebe03a97f9cb5349e07a0d5ea99953b8b17bd
chancestrickland <chance.strickland@gmail.com>
2019-11-04 22:44:44 -0800

    change storybook webpack config to use src files

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,7 +5,11 @@ const packagesDir = path.resolve(__dirname, "../packages");
 const packages = fs.readdirSync(packagesDir);
 
 const alias = packages.reduce((memo, pkg) => {
-  memo[`@reach/${pkg}`] = path.join(packagesDir, pkg);
+  memo[`@reach/${pkg}/styles.css`] = path.join(
+    packagesDir,
+    `${pkg}/styles.css`
+  );
+  memo[`@reach/${pkg}`] = path.join(packagesDir, `${pkg}/src`);
   return memo;
 }, {});
 
