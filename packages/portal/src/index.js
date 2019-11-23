c690f6a80cbfbef600bd6b7a4bd4de715f67209f
chancestrickland <chance.strickland@gmail.com>
2019-11-22 18:12:16 -0800

    portal: replace component-component with hooks

--- packages/portal/src/index.js
+++ packages/portal/src/index.js
@@ -1,31 +1,27 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
-import Component from "@reach/component-component";
 
 function Portal({ children, type = "reach-portal" }) {
-  return (
-    <Component
-      getRefs={() => ({ mountNode: null, portalNode: null })}
-      didMount={({ refs, forceUpdate }) => {
-        // It's possible that the content we are portal has, itself, been portaled.
-        // In that case, it's important to append to the correct document element.
-        const ownerDocument = refs.mountNode.ownerDocument;
-        refs.portalNode = ownerDocument.createElement(type);
-        ownerDocument.body.appendChild(refs.portalNode);
-        forceUpdate();
-      }}
-      willUnmount={({ refs: { portalNode } }) => {
-        portalNode.ownerDocument.body.removeChild(portalNode);
-      }}
-      render={({ refs }) => {
-        const { portalNode } = refs;
-        if (!portalNode) {
-          return <div ref={div => (refs.mountNode = div)} />;
-        } else {
-          return createPortal(children, portalNode);
-        }
-      }}
-    />
+  let mountNode = useRef(null);
+  let portalNode = useRef(null);
+  let [, forceUpdate] = useState();
+
+  useLayoutEffect(() => {
+    // It's possible that the content we are portal has, itself, been portaled.
+    // In that case, it's important to append to the correct document element.
+    const ownerDocument = mountNode.current.ownerDocument;
+    portalNode.current = ownerDocument.createElement(type);
+    ownerDocument.body.appendChild(portalNode.current);
+    forceUpdate({});
+    return () => {
+      portalNode.current.ownerDocument.body.removeChild(portalNode.current);
+    };
+  }, [type]);
+
+  return portalNode.current ? (
+    createPortal(children, portalNode.current)
+  ) : (
+    <div ref={mountNode} />
   );
 }
 
