92c8bd4c3a16403aa78bdadbd669152b3315127d
chancestrickland <chance.strickland@gmail.com>
2019-12-16 15:29:43 -0500

    website: fix menu button label

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -179,7 +179,7 @@ let Nav = ({ media }) => (
               <NavLink to="/alert-dialog">AlertDialog</NavLink>
               <NavLink to="/combobox">Combobox</NavLink>
               <NavLink to="/dialog">Dialog (Modal)</NavLink>
-              <NavLink to="/menu-button">MenuButton (Dropdown)</NavLink>
+              <NavLink to="/menu-button">MenuButton</NavLink>
               <NavLink to="/portal">Portal</NavLink>
               <NavLink to="/skip-nav">SkipNav</NavLink>
               <NavLink to="/slider">Slider</NavLink>
