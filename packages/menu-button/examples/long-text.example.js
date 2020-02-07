769e47cdb046be84088bc075f04e1f90161953f1
chancestrickland <chance.strickland@gmail.com>
2020-02-07 15:12:32 -0800

    popover: adjust default collisions to take button width into account (#460)

--- packages/menu-button/examples/long-text.example.js
+++ packages/menu-button/examples/long-text.example.js
@@ -5,7 +5,7 @@ import {
   MenuList,
   MenuLink,
   MenuButton,
-  MenuItem
+  MenuItem,
 } from "@reach/menu-button";
 import "@reach/menu-button/styles.css";
 
@@ -14,42 +14,36 @@ let name = "Long Text";
 function Example() {
   return (
     <>
-      <Menu>
-        <MenuButton>
-          Developers Developers Developers Developers <span aria-hidden>▾</span>
-        </MenuButton>
-        <MenuList>
-          <MenuItem onSelect={action("Download")}>Download</MenuItem>
-          <MenuItem onSelect={action("Copy")}>Create a Copy</MenuItem>
-          <MenuItem onSelect={action("Mark as Draft")}>Mark as Draft</MenuItem>
-          <MenuItem onSelect={action("Delete")}>Delete</MenuItem>
-          <MenuLink href="https://reacttraining.com/workshops/">
-            Attend a Workshop Attend a Workshop Attend a Workshop Attend a
-            Workshop Attend a Workshop
-          </MenuLink>
-        </MenuList>
-      </Menu>
       <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Menu>
-          <MenuButton>
-            Developers Developers Developers Developers{" "}
-            <span aria-hidden>▾</span>
-          </MenuButton>
-          <MenuList>
-            <MenuItem onSelect={action("Download")}>Download</MenuItem>
-            <MenuItem onSelect={action("Copy")}>Create a Copy</MenuItem>
-            <MenuItem onSelect={action("Mark as Draft")}>
-              Mark as Draft
-            </MenuItem>
-            <MenuItem onSelect={action("Delete")}>Delete</MenuItem>
-            <MenuLink href="https://reacttraining.com/workshops/">
-              Attend a Workshop Attend a Workshop Attend a Workshop Attend a
-              Workshop Attend a Workshop
-            </MenuLink>
-          </MenuList>
-        </Menu>
+        <MyMenu />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <MyMenu />
+      </div>
+      <div>
+        <MyMenu />
       </div>
     </>
+  );
+}
+
+function MyMenu() {
+  return (
+    <Menu>
+      <MenuButton>
+        Developers Developers Developers Developers <span aria-hidden>▾</span>
+      </MenuButton>
+      <MenuList>
+        <MenuItem onSelect={action("Download")}>Download</MenuItem>
+        <MenuItem onSelect={action("Copy")}>Create a Copy</MenuItem>
+        <MenuItem onSelect={action("Mark as Draft")}>Mark as Draft</MenuItem>
+        <MenuItem onSelect={action("Delete")}>Delete</MenuItem>
+        <MenuLink href="https://reacttraining.com/workshops/">
+          Attend a Workshop Attend a Workshop Attend a Workshop Attend a
+          Workshop Attend a Workshop
+        </MenuLink>
+      </MenuList>
+    </Menu>
   );
 }
 
