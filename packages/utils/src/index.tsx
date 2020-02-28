cbaa0e9aca3f789c7029eec48ccb8869caa82749
Chance Strickland <hi@chancedigital.io>
2020-02-28 13:06:39 -0800

    utils: useFocusChange hook

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -385,6 +385,46 @@ export function useConstant<ValueType>(fn: () => ValueType): ValueType {
 }
 
 /**
+ * Detect when focus changes in our document.
+ *
+ * @param handleChange
+ * @param when
+ * @param ownerDocument
+ */
+export function useFocusChange(
+  handleChange: (
+    activeElement: Element | null,
+    previousActiveElement: Element | null,
+    event?: FocusEvent
+  ) => void = console.log,
+  when: "focus" | "blur" = "focus",
+  ownerDocument: Document = document
+) {
+  let lastActiveElement = useRef(ownerDocument.activeElement);
+
+  useEffect(() => {
+    lastActiveElement.current = ownerDocument.activeElement;
+
+    function onChange(event: FocusEvent) {
+      if (lastActiveElement.current !== ownerDocument.activeElement) {
+        handleChange(
+          ownerDocument.activeElement,
+          lastActiveElement.current,
+          event
+        );
+        lastActiveElement.current = ownerDocument.activeElement;
+      }
+    }
+
+    ownerDocument.addEventListener(when, onChange, true);
+
+    return () => {
+      ownerDocument.removeEventListener(when, onChange);
+    };
+  }, [when, handleChange, ownerDocument]);
+}
+
+/**
  * Passes or assigns a value to multiple refs (typically a DOM node). Useful for
  * dealing with components that need an explicit ref for DOM calculations but
  * also forwards refs assigned by an app.
