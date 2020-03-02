3f133bd7a229f6b6338948e3b4c0223e92199862
Austin Wood <3409645+indiesquidge@users.noreply.github.com>
2020-03-02 15:06:54 -0800

    [Combobox] Remove unused __ComboboxPopoverProps type (#496)
    This was replaced by using Partial<PopoverProps> in 628790f

--- packages/combobox/src/index.tsx
+++ packages/combobox/src/index.tsx
@@ -623,15 +623,6 @@ if (__DEV__) {
   ComboboxPopover.displayName = "ComboboxPopover";
 }
 
-export type __ComboboxPopoverProps = {
-  targetRef?: any;
-  positionMatchWidth?: {
-    width: string;
-    left: number;
-    top: string;
-  };
-};
-
 /**
  * @see Docs https://reacttraining.com/reach-ui/combobox#comboboxpopover-props
  */
