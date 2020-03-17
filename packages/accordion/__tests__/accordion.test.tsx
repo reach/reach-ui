37e2fcae062758ab3971e9da51988d2e1d3702e6
Luke Glazebrook <Glazy@users.noreply.github.com>
2020-03-17 20:22:36 +0000

    accordion: add user event & rendering tests

--- packages/accordion/__tests__/accordion.test.tsx
+++ packages/accordion/__tests__/accordion.test.tsx
@@ -9,19 +9,25 @@ import {
   AccordionPanel,
 } from "@reach/accordion";
 
+const AccordionContent = () => (
+  <>
+    <AccordionItem>
+      <AccordionButton>Button One</AccordionButton>
+      <AccordionPanel>Panel One</AccordionPanel>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionButton>Button Two</AccordionButton>
+      <AccordionPanel>Panel Two</AccordionPanel>
+    </AccordionItem>
+  </>
+);
+
 describe("<Accordion />", () => {
   describe("a11y", () => {
     it("should not have basic a11y issues", async () => {
       let { container, getByText } = render(
         <Accordion>
-          <AccordionItem>
-            <AccordionButton>Button One</AccordionButton>
-            <AccordionPanel>Panel One</AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionButton>Button Two</AccordionButton>
-            <AccordionPanel>Panel Two</AccordionPanel>
-          </AccordionItem>
+          <AccordionContent />
         </Accordion>
       );
       let results: AxeResults = null as any;
@@ -35,6 +41,82 @@ describe("<Accordion />", () => {
         results = await axe(container);
       });
       expect(results).toHaveNoViolations();
+    });
+  });
+
+  describe("rendering", () => {
+    it("should open panel as specified by defaultIndex", () => {
+      let { getByText } = render(
+        <Accordion defaultIndex={1}>
+          <AccordionContent />
+        </Accordion>
+      );
+
+      expect(getByText("Panel One")).not.toBeVisible();
+      expect(getByText("Panel Two")).toBeVisible();
+    });
+  });
+
+  describe("user events", () => {
+    it("should change panel on click", () => {
+      let { getByText } = render(
+        <Accordion>
+          <AccordionContent />
+        </Accordion>
+      );
+
+      let panelOneContent = getByText("Panel One");
+
+      expect(panelOneContent).toBeVisible();
+      fireEvent.click(getByText("Button Two"));
+      expect(panelOneContent).not.toBeVisible();
+      expect(getByText("Panel Two")).toBeVisible();
+    });
+
+    it("should call onChange", () => {
+      let mockOnChange = jest.fn();
+      let { getByText } = render(
+        <Accordion onChange={mockOnChange}>
+          <AccordionContent />
+        </Accordion>
+      );
+
+      fireEvent.click(getByText("Button Two"));
+      expect(mockOnChange).toHaveBeenCalledTimes(1);
+    });
+
+    it("should allow collapsing when collapsible", () => {
+      let { getByText } = render(
+        <Accordion collapsible>
+          <AccordionContent />
+        </Accordion>
+      );
+
+      let panelOneContent = getByText("Panel One");
+      let panelOneButton = getByText("Button One");
+
+      expect(panelOneContent).not.toBeVisible();
+      fireEvent.click(panelOneButton);
+      expect(panelOneContent).toBeVisible();
+      fireEvent.click(panelOneButton);
+      expect(panelOneContent).not.toBeVisible();
+    });
+
+    it("should allow multiple when multiple", () => {
+      let { getByText } = render(
+        <Accordion multiple>
+          <AccordionContent />
+        </Accordion>
+      );
+
+      let panelOneContent = getByText("Panel One");
+      let panelTwoContent = getByText("Panel Two");
+
+      expect(panelOneContent).toBeVisible();
+      expect(panelTwoContent).not.toBeVisible();
+      fireEvent.click(getByText("Button Two"));
+      expect(panelOneContent).toBeVisible();
+      expect(panelTwoContent).toBeVisible();
     });
   });
 });
