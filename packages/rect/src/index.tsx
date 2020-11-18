4f74768a0332c07f8ad0ef069b77044a3445c450
Chance Strickland <hi@chancedigital.io>
2020-11-18 12:20:01 -0800

    rect: add examples and update docs

--- packages/rect/src/index.tsx
+++ packages/rect/src/index.tsx
@@ -11,7 +11,12 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import observeRect from "@reach/observe-rect";
-import { useIsomorphicLayoutEffect as useLayoutEffect } from "@reach/utils";
+import {
+  isBoolean,
+  isFunction,
+  useIsomorphicLayoutEffect as useLayoutEffect,
+  warning,
+} from "@reach/utils";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,35 +31,14 @@ export const Rect: React.FC<RectProps> = ({
   children,
 }) => {
   const ref = React.useRef<HTMLElement | null>(null);
-  const rect = useRect(ref, observe, onChange);
+  const rect = useRect(ref, { observe, onChange });
   return children({ ref, rect });
 };
 
 /**
  * @see Docs https://reach.tech/rect#rect-props
  */
-export type RectProps = {
-  /**
-   * Tells `Rect` to observe the position of the node or not. While observing,
-   * the `children` render prop may call back very quickly (especially while
-   * scrolling) so it can be important for performance to avoid observing when
-   * you don't need to.
-   *
-   * This is typically used for elements that pop over other elements (like a
-   * dropdown menu), so you don't need to observe all the time, only when the
-   * popup is active.
-   *
-   * Pass `true` to observe, `false` to ignore.
-   *
-   * @see Docs https://reach.tech/rect#rect-observe
-   */
-  observe?: boolean;
-  /**
-   * Calls back whenever the `rect` of the element changes.
-   *
-   * @see Docs https://reach.tech/rect#rect-onchange
-   */
-  onChange?: (rect: PRect) => void;
+export type RectProps = UseRectOptions & {
   /**
    * A function that calls back to you with a `ref` to place on an element and
    * the `rect` measurements of the dom node.
@@ -82,6 +66,17 @@ if (__DEV__) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+export function useRect<T extends Element = HTMLElement>(
+  nodeRef: React.RefObject<T | undefined | null>,
+  observe?: UseRectOptions["observe"],
+  onChange?: UseRectOptions["onChange"]
+): null | DOMRect;
+
+export function useRect<T extends Element = HTMLElement>(
+  nodeRef: React.RefObject<T | undefined | null>,
+  options?: UseRectOptions
+): null | DOMRect;
+
 /**
  * useRect
  *
@@ -91,14 +86,49 @@ if (__DEV__) {
  */
 export function useRect<T extends Element = HTMLElement>(
   nodeRef: React.RefObject<T | undefined | null>,
-  observe: boolean = true,
-  onChange?: (rect: DOMRect) => void
+  observeOrOptions?: boolean | UseRectOptions,
+  deprecated_onChange?: UseRectOptions["onChange"]
 ): null | DOMRect {
+  let observe: boolean;
+  let onChange: UseRectOptions["onChange"];
+  if (isBoolean(observeOrOptions)) {
+    observe = observeOrOptions;
+  } else {
+    observe = observeOrOptions?.observe ?? true;
+    onChange = observeOrOptions?.onChange;
+  }
+  if (isFunction(deprecated_onChange)) {
+    onChange = deprecated_onChange;
+  }
+
+  if (__DEV__) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    React.useEffect(() => {
+      warning(
+        !isBoolean(observeOrOptions),
+        "Passing `observe` as the second argument to `useRect` is deprecated and will be removed in a future version of Reach UI. Instead, you can pass an object of options with an `observe` property as the second argument (`useRect(ref, { observe })`).\n" +
+          "See https://reach.tech/rect#userect-observe"
+      );
+    }, [observeOrOptions]);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    React.useEffect(() => {
+      warning(
+        !isFunction(deprecated_onChange),
+        "Passing `onChange` as the third argument to `useRect` is deprecated and will be removed in a future version of Reach UI. Instead, you can pass an object of options with an `onChange` property as the second argument (`useRect(ref, { onChange })`).\n" +
+          "See https://reach.tech/rect#userect-onchange"
+      );
+    }, [deprecated_onChange]);
+  }
+
   let [element, setElement] = React.useState(nodeRef.current);
   let initialRectIsSet = React.useRef(false);
   let initialRefIsSet = React.useRef(false);
   let [rect, setRect] = React.useState<DOMRect | null>(null);
-  let onChangeRef = React.useRef<typeof onChange>();
+  let onChangeRef = React.useRef(onChange);
+  let stableOnChange = React.useCallback((rect: PRect) => {
+    onChangeRef.current && onChangeRef.current(rect);
+  }, []);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useLayoutEffect(() => {
@@ -136,7 +166,7 @@ export function useRect<T extends Element = HTMLElement>(
     }
 
     observer = observeRect(elem, (rect) => {
-      onChangeRef.current && onChangeRef.current(rect);
+      stableOnChange(rect);
       setRect(rect);
     });
 
@@ -146,10 +176,37 @@ export function useRect<T extends Element = HTMLElement>(
     function cleanup() {
       observer && observer.unobserve();
     }
-  }, [observe, element, nodeRef]);
+  }, [observe, element, nodeRef, stableOnChange]);
 
   return rect;
 }
+
+/**
+ * @see Docs https://reach.tech/rect#userect
+ */
+export type UseRectOptions = {
+  /**
+   * Tells `Rect` to observe the position of the node or not. While observing,
+   * the `children` render prop may call back very quickly (especially while
+   * scrolling) so it can be important for performance to avoid observing when
+   * you don't need to.
+   *
+   * This is typically used for elements that pop over other elements (like a
+   * dropdown menu), so you don't need to observe all the time, only when the
+   * popup is active.
+   *
+   * Pass `true` to observe, `false` to ignore.
+   *
+   * @see Docs https://reach.tech/rect#userect-observe
+   */
+  observe?: boolean;
+  /**
+   * Calls back whenever the `rect` of the element changes.
+   *
+   * @see Docs https://reach.tech/rect#userect-onchange
+   */
+  onChange?: (rect: PRect) => void;
+};
 
 export default Rect;
 
