0cf39eafc1940941f7866640ed8cd1db0f8642c6
Chance Strickland <hi@chancedigital.io>
2021-01-26 12:13:27 -0800

    clean up deps with manypkg

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,6 @@ module.exports = {
     "babel-plugin-annotate-pure-calls",
     "babel-plugin-dev-expression",
     ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
     "babel-plugin-macros",
   ],
   presets: [
