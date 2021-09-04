d72226c86d2b4d201027556d51adc3ba9d2f8d87
Chance Strickland <chance.strickland@gmail.com>
2021-09-04 09:36:28 -0700

    portal: update docs and warnings for containerRef prop

--- packages/portal/src/index.tsx
+++ packages/portal/src/index.tsx
@@ -15,6 +15,7 @@ import * as React from "react";
 import { useIsomorphicLayoutEffect as useLayoutEffect } from "@reach/utils/use-isomorphic-layout-effect";
 import { useForceUpdate } from "@reach/utils/use-force-update";
 import { createPortal } from "react-dom";
+import warning from "tiny-warning";
 
 /**
  * Portal
@@ -30,14 +31,38 @@ const Portal: React.FC<PortalProps> = ({
   let portalNode = React.useRef<HTMLElement | null>(null);
   let forceUpdate = useForceUpdate();
 
+  if (__DEV__) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    React.useEffect(() => {
+      if (containerRef != null) {
+        warning(
+          typeof containerRef === "object" && "current" in containerRef,
+          "@reach/portal: Invalid value passed to the `containerRef` of a " +
+            "`Portal`. The portal will be appended to the document body, but if " +
+            "you want to attach it to another DOM node you must pass a valid " +
+            "React ref object to `containerRef`."
+        );
+
+        warning(
+          containerRef ? containerRef.current != null : true,
+          "@reach/portal: A ref was passed to the `containerRef` prop of a " +
+            "`Portal`, but no DOM node was attached to it. Be sure to pass the " +
+            "ref to a DOM component.\n\nIf you are forwarding the ref from " +
+            "another component, be sure to use the React.forwardRef API. " +
+            "See https://reactjs.org/docs/forwarding-refs.html."
+        );
+      }
+    }, [containerRef]);
+  }
+
   useLayoutEffect(() => {
     // This ref may be null when a hot-loader replaces components on the page
     if (!mountNode.current) return;
     // It's possible that the content of the portal has, itself, been portaled.
     // In that case, it's important to append to the correct document element.
-    const ownerDocument = mountNode.current!.ownerDocument;
+    let ownerDocument = mountNode.current!.ownerDocument;
+    let body = containerRef?.current || ownerDocument.body;
     portalNode.current = ownerDocument?.createElement(type)!;
-    const body = containerRef?.current || ownerDocument.body;
     body.appendChild(portalNode.current);
     forceUpdate();
     return () => {
@@ -45,7 +70,7 @@ const Portal: React.FC<PortalProps> = ({
         body.removeChild(portalNode.current);
       }
     };
-  }, [type, forceUpdate]);
+  }, [type, forceUpdate, containerRef]);
 
   return portalNode.current ? (
     createPortal(children, portalNode.current)
@@ -71,11 +96,13 @@ type PortalProps = {
    */
   type?: string;
   /**
-   * Optional container ref to render the portal in.
+   * The container ref to which the portal will be appended. If not set the
+   * portal will be appended to the body of the component's owner document
+   * (typically this is the `document.body`).
    *
    * @see Docs https://reach.tech/portal#portal-containerRef
    */
-  containerRef?: React.RefObject<any>;
+  containerRef?: React.RefObject<Node>;
 };
 
 if (__DEV__) {
