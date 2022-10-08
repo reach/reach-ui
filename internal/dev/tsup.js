08023117130a1ab2d5aa6ab719f8bbf267fe8a3c
Chance Strickland <hi@chance.dev>
2022-10-08 11:12:30 -0400

    chore: update license in build

--- internal/dev/tsup.js
+++ internal/dev/tsup.js
@@ -68,7 +68,7 @@ function createBanner(packageName, version) {
 	return `/**
   * ${packageName} v${version}
   *
-  * Copyright (c) React Training
+  * Copyright (c) 2018-${new Date().getFullYear()}, React Training LLC
   *
   * This source code is licensed under the MIT license found in the
   * LICENSE.md file in the root directory of this source tree.
