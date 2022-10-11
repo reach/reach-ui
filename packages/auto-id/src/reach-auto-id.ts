f50908c6203b44032b601307cb5e7b9aece8f4c6
Chance Strickland <hi@chance.dev>
2022-10-11 17:32:44 -0400

    fix(auto-id): fix buggy React 18 fallback implementation

--- packages/auto-id/src/reach-auto-id.ts
+++ packages/auto-id/src/reach-auto-id.ts
@@ -68,8 +68,7 @@ function genId() {
 // Workaround for https://github.com/webpack/webpack/issues/14814
 // https://github.com/eps1lon/material-ui/blob/8d5f135b4d7a58253a99ab56dce4ac8de61f5dc1/packages/mui-utils/src/useId.ts#L21
 const maybeReactUseId: undefined | (() => string) = (React as any)[
-	// eslint-disable-next-line no-useless-concat
-	"useId" + ""
+	"useId".toString()
 ];
 
 /**
