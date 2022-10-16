e10237706f8859e3f07a3879924279f36ba77bc6
Chance Strickland <hi@chance.dev>
2022-10-16 16:05:47 -0700

    deps and junk

--- website/remix.config.js
+++ website/remix.config.js
@@ -1,6 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	ignoredRouteFiles: ["**/.*"],
+	serverDependenciesToBundle: [
+		// MDX junk generally
+		/^rehype.*/,
+		/^remark.*/,
+		/^unified.*/,
+	],
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "build/index.js",
